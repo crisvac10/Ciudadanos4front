9401896ef07d82e2be28ee492303ea8c49451e0b
crisvac10 <82240074+crisvac10@users.noreply.github.com>
2021-12-07 11:13:23 -0500

    Add files via upload

---
+++ Js/Vet.js
@@ -0,0 +1,26 @@
+var username = sessionStorage.getItem("username")
+var url =  "http://localhost:8080/FourPawsCitizens-LazarusAES-25-1.0-SNAPSHOT/api/officials/" + username + "/getOwners"; 
+
+//get data from api for table vet
+fetch(url1)
+  .then(response => response.json())
+  .then(json =>
+    tablaListado=$("#tabla1").DataTable({
+        "data":json,
+        "columns":[
+            {"data":"idVisita"},
+            {"data":"fec.creacion"},
+            {"data":"tipo"},
+            {"data":"descripcion"},
+            {"data":"vetId"},
+            {"data": "petId"}
+        ],
+        "columnDefs": [ {
+          "targets": 6,
+          "data": null,
+          "defaultContent": "2"
+      } ]
+      })
+
+   )
+
